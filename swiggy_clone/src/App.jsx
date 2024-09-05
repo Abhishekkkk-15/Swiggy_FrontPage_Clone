6bdc14cb7bef33edbdc52a8f96fa52940a74978f
Abhishej jangid <mrabhi748@gmail.com>
2024-09-05 13:00:22 +0530

    Clone done

--- swiggy_clone/src/App.jsx
+++ swiggy_clone/src/App.jsx
@@ -1,10 +1,15 @@
 import Header from './components/Header'
-
+import Category from './components/Category'
+import TopRest from './components/TopRest.jsx'
+import OnlineDel from './components/OnlineDel.jsx';
 
 function App() {
   return (
     <>
     <Header/>
+    <Category/>
+    <TopRest/>
+    <OnlineDel/>
     </>
   );
 }
--- swiggy_clone/src/components/Header.jsx
+++ swiggy_clone/src/components/Header.jsx
@@ -16,7 +16,7 @@ export default function Header() {
 
   return (
     <>
-      <div className="black-oerlay w-full h-full fixed duration-500" style={{
+      <div className="black-oerlay w-full h-full fixed duration-500 z-50" style={{
         opacity: toggle ? 1 : 0,
         visibility: toggle ? "visible" : "hidden"
       }} onClick={() => { setToggle(!toggle) }}>
@@ -24,7 +24,7 @@ export default function Header() {
           left: toggle ? "0%" : "-100%"
         }} onClick={(e) => e.stopPropagation()}> <span onClick={() => { setToggle(!toggle) }} className="cursor-pointer text-[45px] text-bold m-l-[20px]">X</span> </div>
       </div>
-      <header className='p-3 shadow-xl text-[#686b78]'>
+      <header className='p-3 shadow-xl text-[#686b78]   bg-white z-[9999]'>
         <div className=" max-w-[1200px] mx-auto flex items-center">
           <div className="w-[100px] " onClick={() => { setToggle(!toggle) }}>
             <img src="https://logos-world.net/wp-content/uploads/2020/11/Swiggy-Emblem.png" alt="" className='w-full' />
